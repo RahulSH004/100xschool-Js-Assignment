df3ef89ed1028099e9dd9443a25c3e740c1c4b14
Hp <rs739025@gmail.com>
2026-01-26 23:03:02 +0530

    hard 1 resolve

--- 01-async-js/easy/2-counter.js
+++ 01-async-js/easy/2-counter.js
@@ -3,3 +3,11 @@
 // Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck.
 
 // (Hint: setTimeout)
+
+let counter = 0;
+const updatecounter=() => {
+    counter++;
+    console.log(counter);
+    setTimeout(updatecounter, 1000);
+}
+updatecounter();
--- 01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ 01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -1,14 +1,13 @@
 /*
     Write a function that returns a promise that resolves after n seconds have passed, where n is passed as an argument to the function.
 */
+const  wait = (n) => {
+  return new Promise((resolve)=> {
+    setTimeout(() => {
+      resolve();
+    },n*1000);
+  })
+}
+module.exports = wait;
 
-function wait(n) {
-    let p = new Promise((resolve) => {
-      setTimeout(() => {
-        resolve();
-      }, n * 1000);
-    });
-    return p;
-  }
-  
-  module.exports = wait;
+
